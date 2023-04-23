ccb2c7ad426bd105f5c10fde1c40e04a45faf03b
孟子文 <mengziwen@tjmeiteng.com>
2023-04-23 16:22:38 +0800

    设备状态

--- src/component/scraper/state.ts
+++ src/component/scraper/state.ts
@@ -1,71 +1,35 @@
 export default [
   {
-    rect1: {
-      bj: ["#777F89", "#EEEFF1", "#575F69"],
-      border: "#000000",
+    middle: {
+      color: "#D4D9DF",
     },
-    rect2: {
-      bj: ["#EEEFF1"],
-      border: "#000000",
-    },
-    rect3: {
-      bj: ["#E3E3E3", "#FFFFFF", "#B0B0B0"], // #B0B0B0
-      border: "#88909A",
-    },
-    round: {
-      bj: ["#88909A"],
+    img: {
+      red: 0,
     },
   },
   {
-    rect1: {
-      bj: ["#55DBA8", "#92E6C7", "#197D50"],
-      border: "#000000",
+    middle: {
+      color: "#52DDAB",
     },
-    rect2: {
-      bj: ["#EEEFF1"],
-      border: "#000000",
-    },
-    rect3: {
-      bj: ["#E3E3E3", "#FFFFFF", "#B0B0B0"],
-      border: "#2FB790",
-    },
-    round: {
-      bj: ["#52DDAB"],
+    img: {
+      green: 50,
     },
   },
 
   {
-    rect1: {
-      bj: ["#6C0A0A", "#FC8181", "#BA3636"],
-      border: "#000000",
+    middle: {
+      color: "#EA8080",
     },
-    rect2: {
-      bj: ["#EEEFF1"],
-      border: "#000000",
-    },
-    rect3: {
-      bj: ["#E3E3E3", "#FFFFFF", "#B0B0B0"],
-      border: "#B72525",
-    },
-    round: {
-      bj: ["#F04C4C"],
+    img: {
+      red: 50,
     },
   },
   {
-    rect1: {
-      bj: ["#305EB6", "#8AB8FA", "#5A8EF6"],
-      border: "#000000",
+    middle: {
+      color: "#8AB8FA",
     },
-    rect2: {
-      bj: ["#EEEFF1"],
-      border: "#000000",
-    },
-    rect3: {
-      bj: ["#E3E3E3", "#FFFFFF", "#B0B0B0"],
-      border: "#5A8EF6",
-    },
-    round: {
-      bj: ["#629DE3"],
+    img: {
+      blue: 50,
     },
   },
 ];
